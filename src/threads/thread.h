dd46b6d95a39ed872e5841ba28af87677ad418d9
Zaynstir <Zaynstir@gmail.com>
2020-03-06 16:07:50 -0600

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -89,6 +89,8 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
+    long toExpire;
+    struct list_elem telem;
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
