28872ecd8a06dd1c4007ef2af5e9c4975c9c1d38
gness9 <glenn.n@ravenengineer.org>
2020-03-08 19:35:41 -0500

    Fixing Cheese

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -48,7 +48,7 @@ syscall_handler (struct intr_frame *f UNUSED)
   }
   
   printf("\nCCCCCCC\n");
-  printf("BLARGH: %d\n, %d", *((int*)f->esp), SYS_EXIT);
+  printf("BLARGH: %d, %d", *((int*)f->esp+1), SYS_EXIT);
   //int * args = f->esp;
   
   switch(*(int*)f->esp) 
